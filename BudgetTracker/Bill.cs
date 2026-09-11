a9e3b979076ab29a5cc3794dbc937aca199cd38d
Jackson Young <jacksonyoung1@hotmail.com>
2026-09-12 06:17:58 +1000

    Create Bill Class for v0.2.0

---
+++ BudgetTracker/Bill.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetTracker
+{
+    public class Bill
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
